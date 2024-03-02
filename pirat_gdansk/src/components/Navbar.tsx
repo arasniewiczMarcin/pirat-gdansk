7b843d2c677633aaac986ded1d2b9311b8a47fd7
Jan <janszczepkowsky@gmail.com>
2024-03-02 09:35:27 +0100

    kolory, podswietlane przyciski

--- pirat_gdansk/src/components/Navbar.tsx
+++ pirat_gdansk/src/components/Navbar.tsx
@@ -5,14 +5,14 @@ const Navbar = () => {
     return (
         <nav className="bg-[#FFB923]">
                 <div className="flex items-center justify-center space-x-16 ">
-                    <Link to="/o_nas" className="text-white hover:text-gray-300">O nas</Link>
-                    <Link to="/kadra" className="text-white hover:text-gray-300">Kadra</Link>
+                    <Link to="/o_nas" className="text-gray hover:bg-[#ff9923]">O nas</Link>
+                    <Link to="/kadra" className="text-gray hover:bg-[#ff9923]">Kadra</Link>
                     
-                    <Link to="/" className="text-white hover:text-gray-300">
-                        <img src={logo} alt='logo pirata' width={150}/>
+                    <Link to="/" className="text-gray hover:text-gray-300">
+                        <img src={logo} alt='logo pirata' width={100}/>
                     </Link>
-                    <Link to="/galeria" className="text-white hover:text-gray-300">Galeria</Link>
-                    <Link to="/sponsorzy" className="text-white hover:text-gray-300">Sponsorzy</Link>
+                    <Link to="/galeria" className="text-gray hover:bg-[#ff9923]">Galeria</Link>
+                    <Link to="/sponsorzy" className="text-gray hover:bg-[#ff9923]">Sponsorzy</Link>
                 </div>
         </nav>
     );
--- pirat_gdansk/tailwind.config.js
+++ pirat_gdansk/tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   theme: {
     colors: {
       'yellow': '#FFB923',
+      'black': '#000000',
+      'white': '#ffffff',
+      'gray':'#1a1a1a',
     },
     extend: {},
   },
